98535c0ef5a6efd056a9458b2ef1279f7888f80b
Adrian Booth <adrian.booth@yourgolftravel.com>
2016-04-29 21:05:47 +0100

    Small changes with Vicky

--- Gemfile
+++ Gemfile
@@ -1,9 +1,7 @@
 source 'https://rubygems.org'
-ruby '2.1.5'
+ruby '2.1.6'
 
 gem 'sinatra'
-gem 'pony'
-gem 'stripe'
 
 #
 # group :test do
--- app/site_web.rb
+++ app/site_web.rb
@@ -1,42 +1,15 @@
 require 'sinatra'
-require 'pony'
-require 'stripe'
 require "bundler/setup"
 
-set :publishable_key, ENV['PUBLISHABLE_KEY']
-set :secret_key, ENV['SECRET_KEY']
-Stripe.api_key = settings.secret_key
+
+
+
 
 
 class SiteApp < Sinatra::Base
 
   get "/" do
       erb :index
-  end
-
-  post '/' do
-        name = params[:name]
-        mail = params[:mail]
-        subject = params[:subject]
-        body = params[:body]
-        Pony.mail(:to => 'Adrian.booth17@gmail.com', :from => "#{mail}", :subject => "#{subject}", :body => "#{body}")
-        erb :index
-  end
-
-  post '/charge' do
-    @amount = 500
-    customer = Stripe::Customer.create(
-      :email => params[:email],
-      :card => params[:stripeToken]
-    )
-
-    charge = Stripe.Charge.create(
-      :amount => params[:amount],
-      :description => 'Sinatra Charge',
-      :currency => 'gbp',
-      :customer => customer.id
-    )
-    erb :charge
   end
 
 end
